60478262d01a28e2abbca8ae2d22c2212af19282
FedericoCabrera <fedegcab@gmail.com>
2017-04-17 18:09:15 -0300

    Add Commit test 1

--- Lupi.Data.Entities/Pet.cs
+++ Lupi.Data.Entities/Pet.cs
@@ -8,7 +8,7 @@ namespace Lupi.Data.Entities
 {
     public class Pet
     {
-        //Pet comment 2
+        //Pet comment 2 1
         public Guid Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
